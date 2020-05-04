56ef8adfdf6ff4e3fdb597b6570b9a8a60ddb996
simphiwehlabisa <sfisohlabisa@gmail.com>
2020-05-04 02:15:45 +0200

    Add Controller To Main Router File

--- controllers/books.go
+++ controllers/books.go
@@ -2,5 +2,17 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/simphiwehlabisa/go-crud-api/models"
 )
+
+// GET /books
+// get all books
+func FindBooks(c *gin.Context) {
+	db := c.Mustget("db").(*gorm.DB)
+
+	var books []models.Book
+	db.find(&books)
+
+	c.Json(http.StatusOk, gin.H{"data": books})
+}
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
+	"github.com/simphiwehlabisa/go-crud-api/controllers"
 	"github.com/simphiwehlabisa/go-crud-api/models"
 )
 
@@ -14,16 +17,18 @@ func main() {
 		c.Next()
 	})
 
-	// router.LoadHTMLGlob("templates/*")
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.HTML(
-	// 		http.StatusOK,
-	// 		"index.html",
-	// 		gin.H{
-	// 			"title": "Home Page",
-	// 		},
-	// 	)
-	// })
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	router.GET("/books", controllers.FindBooks)
 
 	router.Run("localhost:8082")
 }
