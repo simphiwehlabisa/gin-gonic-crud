8cb613c5203a307662c7c08ee87e5b7e589b0d96
simphiwehlabisa <sfisohlabisa@gmail.com>
2020-05-04 12:11:14 +0200

    FIx Env

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func goDotEnvVariable(key string) string {
 
 	// load .env file
-	err := godotenv.Load(".env")
+	err := godotenv.Load()
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
